33d4962d5e2ee10cd11e8ef1517456cd9a683171
natali88988 <nkovarskaya98@mail.ru>
2022-09-23 13:17:40 +0300

    Homework 1

---
+++ task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) Console.WriteLine($"Наибольшее число: {a}, наименьшее число: {b}");
+else 
+{  if (a<b) Console.WriteLine($"Наибольшее число: {b}, наименьшее число: {a}");
+   else Console.WriteLine($"Числа {a} и {b} равны");
+}
---
+++ task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>=b && a>c) Console.WriteLine($"Наибольшее число: {a}");
+if (b>a && b>=c) Console.WriteLine($"Наибольшее число: {b}");
+if (c>=a && c>b) Console.WriteLine($"Наибольшее число: {c}");
+if (a==b && b==c) Console.WriteLine($"Числа {a}, {b} и {c} равны");
